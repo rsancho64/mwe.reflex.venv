05fbb1d117f174d5a8ddeb9a04b2ff67223b7dbe
rsancho64 <rsancho@iesrioarba.es>
2024-01-15 12:39:08 +0100

    .

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # MWE.reflex.con.venv.y.makefile
 
-cd my_app_name
+- [ ] [read](https://medium.com/@HeCanThink/reflex-a-library-to-build-performant-and-customizable-web-apps-in-pure-python-2bfde0344af2) 
 
+## build flow
+
+mkdir my_app_name
+cd my_app_name
 python3 -m venv .venv
 echo reflex >> requirements.txt
 source .venv/bin/activate
+
 # pip install reflex --use-deprecated=legacy-resolver
 reflex init # TODO: plantilla minima ? (basic)
--- app0/app0/app0.py
+++ app0/app0/app0.py
@@ -7,19 +7,11 @@ class State(rx.State):
     itemName = ["tomates", "patatas", "cebollas"]
     count = [0,0,0,]
 
+    def incrementItem(self, intemindex):
+        self.count[intemindex] += 1
 
-    def incrementTomates(self, pos):
-        self.count[pos] += 1
-
-    def decrementTomates(self):
-        self.countTomates -= 1
-
-    def incrementPatatas(self):
-        self.countPatatas += 1
-
-    def decrementPatatas(self):
-        self.countPatatas -= 1
-
+    def incrementItem(self, intemindex):
+        self.count[intemindex] -= 1
 
 # def newCustomized_rx_hstack():
 #     return rx.hstack(
@@ -40,51 +32,55 @@ class State(rx.State):
 #     )
 
 def index():
-    return rx.vstack(
-        rx.hstack(
-            rx.text("patatas", font_size="2em"),
-            rx.heading(State.countPatatas, font_size="2em"),
-        ),
-        rx.hstack(
-            rx.button(
-                "--",
-                bg="#fef2f2",
-                color="#b91c1c",
-                border_radius="lg",
-                on_click=State.decrementPatatas,
-            ),
-            rx.button(
-                "++",
-                bg="#ecfdf5",
-                color="#047857",
-                border_radius="lg",
-                on_click=State.incrementPatatas,
-            ),
-        ),
-        # spacing="1em",
-        rx.hstack(
-            rx.text("tomates", font_size="2em"),
-            rx.heading(State.countTomates, font_size="2em"),
-        ),
-        rx.hstack(
-            rx.button(
-                "--",
-                bg="#fef2f2",
-                color="#b91c1c",
-                border_radius="lg",
-                on_click=State.decrementTomates,
-            ),
-            rx.button(
-                "++",
-                bg="#ecfdf5",
-                color="#047857",
-                border_radius="lg",
-                on_click=State.incrementTomates,
-            ),
-        ),
-        # # spacing="1em",
+    
+    retorno = [] # ???
 
-    )
+    # return rx.vstack(
+    #     rx.
+    #     rx.hstack(
+    #         rx.text("patatas", font_size="2em"),
+    #         rx.heading(State.countPatatas, font_size="2em"),
+    #     ),
+    #     rx.hstack(
+    #         rx.button(
+    #             "--",
+    #             bg="#fef2f2",
+    #             color="#b91c1c",
+    #             border_radius="lg",
+    #             on_click=State.decrementPatatas,
+    #         ),
+    #         rx.button(
+    #             "++",
+    #             bg="#ecfdf5",
+    #             color="#047857",
+    #             border_radius="lg",
+    #             on_click=State.incrementPatatas,
+    #         ),
+    #     ),
+    #     # spacing="1em",
+    #     rx.hstack(
+    #         rx.text("tomates", font_size="2em"),
+    #         rx.heading(State.countTomates, font_size="2em"),
+    #     ),
+    #     rx.hstack(
+    #         rx.button(
+    #             "--",
+    #             bg="#fef2f2",
+    #             color="#b91c1c",
+    #             border_radius="lg",
+    #             on_click=State.decrementTomates,
+    #         ),
+    #         rx.button(
+    #             "++",
+    #             bg="#ecfdf5",
+    #             color="#047857",
+    #             border_radius="lg",
+    #             on_click=State.incrementTomates,
+    #         ),
+    #     ),
+    #     # # spacing="1em",
+
+    # )
 
 
 app = rx.App()
